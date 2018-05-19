8c1dc0ca59dbaf90fca6a455c3d980cf1fe15ea3
Tapi0 <Tapi0nfriends@gmail.com>
2018-05-19 12:10:41 -0400

    Further C# testing... Fixed Player.cs...  :|

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,15 +1,16 @@
-using System;
-namespace Player
-{
-  class Hello
-  {
-    /// <summary>
-    ///   The main entry point for the application
-    /// </summary>
-    [STAThread]
-    public static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World");
-    }
-  }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello", gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
 }
