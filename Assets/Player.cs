784ff6b5fd757731a697295c0a7760fcaa76c073
Tapi0 <Tapi0nfriends@gmail.com>
2018-05-19 11:23:08 -0400

    Testing C# syncing.

---
+++ Assets/Player.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Player
+{
+  class Hello
+  {
+    /// <summary>
+    ///   The main entry point for the application
+    /// </summary>
+    [STAThread]
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World");
+    }
+  }
+}
